9b1a4bff59d26f0d0b9ebe293f6d0bad08f3e037
Alprod <alprod81@gmail.com>
2022-09-27 10:21:23 +0200

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,7 +8,7 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
-use ApiPlatform\Metadata\Put;
+use App\Controller\RegisterUserController;
 use App\Repository\UserRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
@@ -31,6 +31,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 			name : 'api-details-users'),
 		new Post(
 			uriTemplate : '/register/users',
+			controller: RegisterUserController::class,
 			normalizationContext : [ 'groups' => 'register'],
 			name : 'api-register-users' ),
 		new Patch(
