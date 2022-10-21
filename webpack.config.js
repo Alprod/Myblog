8309e4dcb47218797c21c39f1d72b3146c6ffe66
Alprod <alprod81@gmail.com>
2022-10-21 23:53:50 +0200

    add webpack encore and React

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/react/app.js')
+    .addStyleEntry('style', './assets/styles/app.css')
+
+    .splitEntryChunks()
+
+    .enableSingleRuntimeChunk()
+
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+
+    // .configureBabel((config) => {
+    //     config.plugins.push('@babel/a-babel-plugin');
+    // })
+
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = '3.23';
+    })
+
+    // enables Sass/SCSS support
+    .enableSassLoader()
+
+    // uncomment if you use React
+    .enableReactPreset()
+
+;
+
+module.exports = Encore.getWebpackConfig();
